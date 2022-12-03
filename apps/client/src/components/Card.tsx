fab2db3b3d2c26f6584cd22370a3ca18eb19de8b
notEpsilon <ibrahimnasr393@gmail.com>
2022-12-03 05:05:36 +0200

    finished homepage ui components closes (#1)

--- apps/client/src/components/Card.tsx
+++ apps/client/src/components/Card.tsx
@@ -1,14 +1,23 @@
 interface Props {
   title: string;
   body: string;
+  grad?: "green";
 }
 
-const Card: React.FC<Props> = ({ title, body }) => {
+const Card: React.FC<Props> = ({ title, body, grad }) => {
   return (
-    <div className="block max-w-sm p-6 bg-white border border-pink-300 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div
+      className={`block max-w-sm p-6 bg-white border ${
+        grad === "green" ? "border-emerald-300" : "border-pink-300"
+      } rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700`}
+    >
       <h5 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {title}
-        <div className="h-1 mt-3 w-1/3 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full" />
+        <div
+          className={`h-1 mt-3 max-w-[33.333333%] bg-gradient-to-r ${
+            grad === "green" ? "from-emerald-500" : "from-purple-500"
+          } ${grad === "green" ? "to-cyan-500" : "to-pink-500"} rounded-full`}
+        />
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">{body}</p>
     </div>
